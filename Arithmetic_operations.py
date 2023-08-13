975c8e1a46ff440769585f6067a5fd7e9240d84b
22A91A04M2 <119721144+22A91A04M2@users.noreply.github.com>
2023-08-13 22:37:00 +0530

    a = int(input()) b = int(input()) s = a+b d = a-b p = a*b q = a//b r = a%b print("Sum:{}".format(s)) print("Difference:{}".format(d)) print("Product:{}".format(p)) print("Quotient:{}".format(q)) print("Remainder:{}".format(r))

---
+++ Arithmetic_operations.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+s = a+b
+d = a-b
+p = a*b
+q = a//b
+r = a%b
+print("Sum:{}".format(s))
+print("Difference:{}".format(d))
+print("Product:{}".format(p))
+print("Quotient:{}".format(q))
+print("Remainder:{}".format(r))
